8090cf119620199806a97e4f27ef55e197a10f95
Salona Paliwal <50885039+saloo1234@users.noreply.github.com>
2019-06-18 12:11:02 +0530

    Update script.js

--- module-4-solution/script.js
+++ module-4-solution/script.js
@@ -8,7 +8,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-( function (window) {
+( function () {
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i in names) {
         var firstLetter = (names[i]).charAt(0);
@@ -19,7 +19,7 @@ WARNING!!! WARNING!!!
         }
     }
 
-}) (window);
+}) ();
 
 
 // STEP 10:
