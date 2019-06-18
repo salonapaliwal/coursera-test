a5b11f42f0764e57e59d533e64bfd5e872a03ff4
Salona Paliwal <50885039+saloo1234@users.noreply.github.com>
2019-06-18 12:09:50 +0530

    Update SpeakGoodBye.js

--- module-4-solution/SpeakGoodBye.js
+++ module-4-solution/SpeakGoodBye.js
@@ -3,7 +3,7 @@
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
-( function (window) {
+( function () {
     var byeSpeaker={};
     var speakWord = "Good Bye";
     byeSpeaker.speak= function (name) {
@@ -18,7 +18,7 @@
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 
 window.byeSpeaker = byeSpeaker;
-}) (window);
+}) ();
 
 
 
